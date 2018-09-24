f47eb072bc017a384a0616248120b2f87f5ae444
jobayermojumder <jobayer.pro@gmail.com>
2018-09-24 10:25:47 +0600

    php execution file added

--- SMTP-mail/mail.py
+++ SMTP-mail/mail.py
@@ -1,32 +1,36 @@
 import smtplib
 import csv
 
+
 def mail(email):
     recevier = email
-    SUBJECT = 'TEST MAIL'
-    message = 'Here is a message from python.'
+    SUBJECT = 'Result of Written Examination for the position of Management Trainee Officer of Mercantile Bank Limited.'
+
+    message = 'Dear Sir/Madam,\r\n\r\nPlease find your written examination result in your User Login for Management Trainee Officer position of Mercantile Bank Limited.\r\n\r\nLink : http://www.mblbd.com/career/mycv/login\r\n\r\nThanking you.\r\n\r\nSincerely yours,\r\n\r\nMohammad Iqbal Rezwan\r\nSenior Executive Vice President &\r\nHead of HR'
 
     gmail_sender = 'hrd@mblbd.com'
-    gmail_passwd = 'Hrd@1234'
+    gmail_passwd = 'Mblhrd@147'
 
     server = smtplib.SMTP('webmail.mblbd.com', 587)
     server.ehlo()
     server.login(gmail_sender, gmail_passwd)
 
-    BODY = '\r\n'.join(['TO: %s' % recevier,
-                        'From: %s' % gmail_sender,
+    body = '\r\n'.join(['TO: %s' % recevier,
+                        'From: %s' % 'Mercantile Bank Limited <'+gmail_sender+'>',
                         'Subject: %s' % SUBJECT,
                         '', message])
 
     try:
-        server.sendmail(gmail_sender, [recevier], BODY)
+        server.sendmail(gmail_sender, [recevier], body)
         print ('email sent : ', email)
     except:
-        print ('error sending mail : ',email)
+        print ('error sending mail : ', email)
 
     server.quit()
-    
+
+
 with open('mailaddress.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV:
         mail(row[0])
+
---
+++ php/php7.sh
@@ -0,0 +1,5 @@
+# @Author: Jobayer Mojumder
+# @Date:   2018-09-24 10:23:40
+# @Last Modified by:   jobayer
+# @Last Modified time: 2018-09-24 10:24:06
+sudo a2dismod php5.6 && sudo a2enmod php7.0 && sudo update-alternatives --set php /usr/bin/php7.0 && sudo service apache2 restart
