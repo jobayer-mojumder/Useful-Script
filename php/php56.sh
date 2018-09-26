cce62b3495ec100eb3a4f0ffbbbfb632c4ab7689
jobayermojumder <jobayer.pro@gmail.com>
2018-09-26 14:44:31 +0600

    mysql root password change cmd added

--- php/php56.sh
+++ php/php56.sh
@@ -3,4 +3,4 @@
 # @Last Modified by:   jobayer
 # @Last Modified time: 2018-09-24 10:23:22
 
-sudo a2dismod php7.0 && sudo a2enmod php5.6 && sudo update-alternatives --set php /usr/bin/php5.6 && sudo service apache2 restart
+sudo a2dismod php7.1 && sudo a2enmod php5.6 && sudo update-alternatives --set php /usr/bin/php5.6 && sudo service apache2 restart
--- php/php71.sh
+++ php/php71.sh
@@ -2,4 +2,4 @@
 # @Date:   2018-09-24 10:23:40
 # @Last Modified by:   jobayer
 # @Last Modified time: 2018-09-24 10:24:06
-sudo a2dismod php5.6 && sudo a2enmod php7.0 && sudo update-alternatives --set php /usr/bin/php7.0 && sudo service apache2 restart
+sudo a2dismod php5.6 && sudo a2enmod php7.1 && sudo update-alternatives --set php /usr/bin/php7.1 && sudo service apache2 restart
